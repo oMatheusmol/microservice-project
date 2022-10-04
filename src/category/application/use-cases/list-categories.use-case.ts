cd7b224ed79c9a8902f60f1d75489131c6f73831
Matheus Mol <matheusmol@hotmail.com>
2022-10-04 20:50:31 +0000

    feat: :art: list category use case

---
+++ src/category/application/use-cases/list-categories.use-case.ts
@@ -0,0 +1,33 @@
+import { CategoryRepository } from '../../domain/repository/category.repository';
+import { CategoryOutput, CategoryOutputMapper } from '../dto/category-output';
+import { default as DefaultUseCase } from '#seedwork/application/use-case/use-case';
+import { SearchInputDto } from '#seedwork/application/dto/search-input';
+import {
+  PaginationOutputDto,
+  PaginationOutputMapper,
+} from '#seedwork/application/dto/pagination-output'; 
+
+export namespace ListCategoriesUseCase {
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private categoryRepo: CategoryRepository.Repository) {}
+    //
+    async execute(input: Input): Promise<Output> {
+      const params = new CategoryRepository.SearchParams(input);
+      const searchResult = await this.categoryRepo.search(params);
+      return this.toOutput(searchResult);
+    }
+
+    private toOutput(searchResult: CategoryRepository.SearchResult): Output {
+      const items = searchResult.items.map((i) => {
+        return CategoryOutputMapper.toOutput(i);
+      });
+      return PaginationOutputMapper.toOutput(items, searchResult);
+    }
+  }
+
+  export type Input = SearchInputDto;
+
+  export type Output = PaginationOutputDto<CategoryOutput>;
+}
+
+export default ListCategoriesUseCase;
